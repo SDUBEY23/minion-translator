7d39b245303eb7199b3b99b17cd8b1c9b6cf3ad9
Swapnil <swapnil.vishu.7027@gmail.com>
2021-03-07 00:47:47 +0530

    added css

--- script.js
+++ script.js
@@ -4,30 +4,28 @@ var outputDiv = document.querySelector('#output');
 
 const serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(txtInput){
+function getTranslationURL(txtInput) {
     return serverURL + "?" + "text=" + txtInput;
 }
 
-function errorHandler(error){
+function errorHandler(error) {
     console.log("error occured", error);
     alert("Something Went wrong! Please try after sometime");
 }
 
-function clickEventHandler(){
+function clickEventHandler() {
 
     var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
-    })
-    .catch(errorHandler)
-    
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
-
-
